0adde88e6cb1f4e3e70b403e26d30b7960df7ef9
Janner Gonzalez <76578696+Janner-GP@users.noreply.github.com>
2024-01-04 10:04:16 -0500

    Rutas basicas configuradas

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screen/buttons/button_screen.dart';
 
-class HomeScreeen extends StatelessWidget{
-
+class HomeScreeen extends StatelessWidget {
   const HomeScreeen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Widgets App'),
-      ),
+        appBar: AppBar(
+          title: const Text('Widgets App'),
+        ),
+        body: _HomeView());
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+
+        final item = appMenuItems[index];
+
+        return _CustomListTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      title: Text(item.title),
+      subtitle: Text(item.subtitle),
+      leading: Icon(item.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios, color: colors.primary),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen(),
+          ),
+        );
+      },
     );
   }
 }
